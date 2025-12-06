85a0b3934a08f96ab8d1783503e1a48990ed0acf
Thibault LATXAGUE <thibault.latxague@exakis-nelite.com>
2025-12-06 13:59:07 +0100

    Ajout classe enum et modification joueur

--- src/main/java/game/player/Player.java
+++ src/main/java/game/player/Player.java
@@ -1,26 +1,57 @@
 package game.player;
+import game.utils.Enums.*;
 
 public class Player {
-    private float xCoordinate;
-    private float yCoordinate;
+    private float x;
+    private float y;
+    private float height; // Hauteur
+    private float width; // Largeur
+    private float health; // Vie
+    private float maxHealth; // Vie maximale
     private String name;
+    private Direction direction;
 
     public Player(float x, float y, String name){
-        this.xCoordinate = x;
-        this.yCoordinate = y;
+        this.x = x;
+        this.y = y;
         this.name = name;
     }
 
-    public float getX(){
-        return xCoordinate;
+    public void takeDamage(int amount){
+        if(amount >= getHealth()){
+            setHealth(0);
+        } else {
+            setHealth(health - amount);
+        }
     }
 
-    public float getY(){
-        return yCoordinate;
+    public void heal(int amount){
+        // Return the smallest value of the two floats
+        setHealth(Math.min(amount + getHealth(), getMaxHealth()));
     }
+
+    public boolean isAlive(){
+        return getHealth() == 0f;
+    }
+
+    // ========================
+    // OPTIONS
+    // ========================
+
+    public float getX(){ return x; }
+    public float getY(){ return y; }
+    public float getHeight() { return this.height; }
+    public float getWidth() { return this.width; }
+    public float getHealth() { return this.health; }
+    public float getMaxHealth() { return this.maxHealth; }
+    public Direction getDirection() { return this.direction; }
 
     public void setPosition(float x, float y){
-        xCoordinate = x;
-        yCoordinate = y;
+        this.x = x;
+        this.y = y;
     }
+
+    public void setHealth(float h){ this.health = h; }
+    public void setHeight(float h){ this.height = h; }
+    public void setWidth(float w){ this.width = w; }
 }
---
+++ src/main/java/game/utils/Enums.java
@@ -0,0 +1,10 @@
+package game.utils;
+
+public class Enums {
+    public enum Direction{
+        RIGHT,
+        LEFT,
+        UP,
+        DOWN
+    }
+}
